13126ba254ba7fb2469fbd1bad8cdccc7cafe8c2
Jasir <k250701@nu.edu.pk>
2025-09-13 20:15:23 +0500

    Add files via upload

---
+++ Answer_4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int age, salary;
+
+    printf("Enter your age:\n");
+    scanf("%d", &age);
+    printf("Enter your salary:\n");
+    scanf("%d", &salary);
+
+    if (salary >= 40000 && age >= 25 ) {
+        printf("Loan approved");
+    }else{
+        printf("Loan not approved");
+    }
+
+}
