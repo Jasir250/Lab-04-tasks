ef79101cba077127523fc1eac38da68144ac4c5a
Jasir <k250701@nu.edu.pk>
2025-09-13 21:39:04 +0500

    Add files via upload

---
+++ Answer_5.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main() {
+    int num_1, num_2;
+    char operation;
+
+    printf("Welcome to the calculator program!");
+
+    printf("Enter your first number:\n");
+    scanf("%d", &num_1);
+
+    printf("Enter your second number:\n");
+    scanf("%d", &num_2);
+
+    printf("Select your operation.\n    Enter '+' for addition\n    Enter '-' for subtraction\n    Enter '*' or 'x' for multiplicaton\n    Enter '/' for division\n");
+    scanf(" %c", &operation);
+    
+
+    switch (operation)
+    {
+    case '+':
+        printf("The answer is: %d", num_1 + num_2);
+        break;
+    case '-':
+        printf("The answer is: %d", num_1 - num_2);
+        break;
+    case '*':
+        printf("The answer is: %d", num_1 * num_2);
+        break;
+    case '/':
+        printf("The answer is: %d", num_1 / num_2);
+        break;
+    case 'x':
+        printf("The answer is: %d", num_1 * num_2);
+        break;
+
+    default:
+        printf("Error. Please enter a valid operator");
+        break;
+    }
+
+    return 0;
+}
